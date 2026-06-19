0a2b3b06d750121d974a819ee2aa404d6b0290fe
Oubaida Akilan <obieda.jehad@gmail.com>
2026-06-20 00:45:17 +0300

    Update production environment API URL to point to the live server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://biomunmmxfsvvksmcezm.supabase.co',
   supabaseKey: 'sb_publishable_Ya0SadPrLz85HJvrwtCAmw_GgexxvuH',
-  apiUrl: 'http://localhost:3001/api',
+  apiUrl: ' https://karen-chocolate-subdivision-psychiatry.trycloudflare.com',
 };
