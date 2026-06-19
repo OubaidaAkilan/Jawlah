678cec8c8cfea00376e784ad237540d29ebf941a
Oubaida Akilan <obieda.jehad@gmail.com>
2026-06-20 00:55:49 +0300

    Update production environment API URL to include the '/api' path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://biomunmmxfsvvksmcezm.supabase.co',
   supabaseKey: 'sb_publishable_Ya0SadPrLz85HJvrwtCAmw_GgexxvuH',
-  apiUrl: ' https://karen-chocolate-subdivision-psychiatry.trycloudflare.com',
+  apiUrl: 'https://karen-chocolate-subdivision-psychiatry.trycloudflare.com/api',
 };
