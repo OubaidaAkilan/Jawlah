fb7a688a9574f7efc91b411ef1cbc880679539a8
Oubaida Akilan <obieda.jehad@gmail.com>
2026-06-23 00:27:09 +0300

    Update production environment API URL to the new endpoint for improved service integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://biomunmmxfsvvksmcezm.supabase.co',
   supabaseKey: 'sb_publishable_Ya0SadPrLz85HJvrwtCAmw_GgexxvuH',
-  apiUrl: ' https://andy-docs-variables-indexes.trycloudflare.com/api',
+  apiUrl: 'https://stops-san-oak-settle.trycloudflare.com/api',
 };
